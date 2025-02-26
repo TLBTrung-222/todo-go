a929473afa577418f54ae80c700f1cae462efce6
TrungTLB <baotrung2852003@gmail.com>
2025-02-26 17:02:59 +0700

    config cors middleware

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"github.com/TLBTrung-222/todo-go/handlers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main(){
 	router := gin.Default()
-	
+
+	// config cors for FE which deployed at Vercel
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"https://todo-go-gin.vercel.app/"} 
+	router.Use(cors.New(config)) // use the cors middleware
+
 	router.GET("/tasks", handlers.GetTasks)
 	router.POST("/tasks", handlers.CreateTask)
 	router.GET("/tasks/:id", handlers.GetTask)
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,9 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    server: {
-        proxy: {
-            "/tasks": "https://todo-go-g2zv.onrender.com",
-        },
-    },
 });
