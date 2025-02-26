1e0973251d7f8eb3d847b3797e7ffd6b0ae106d4
TrungTLB <baotrung2852003@gmail.com>
2025-02-26 17:30:20 +0700

    config cors

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/TLBTrung-222/todo-go/handlers"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -11,8 +13,10 @@ func main(){
 
 	// config cors for FE which deployed at Vercel
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"https://todo-go-gin.vercel.app/"} 
+	config.AllowOrigins = []string{"https://todo-go-gin.vercel.app"} 
 	router.Use(cors.New(config)) // use the cors middleware
+	log.Println("CORS configured with:", config)
+
 
 	router.GET("/tasks", handlers.GetTasks)
 	router.POST("/tasks", handlers.CreateTask)
